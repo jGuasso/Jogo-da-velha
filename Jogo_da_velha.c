d7ef926a4eeca5330eb582dded3226c932ffd98f
jGuasso <joao.guasso@acad.ufsm.br>
2024-04-06 10:53:27 -0300

    Reformulação
    Exclui a antiga e dei o nome para a nova

--- Jogo_da_velha.c
+++ Jogo_da_velha.c
@@ -3,36 +3,81 @@
 
 //Projeto de jogo da velha
 int main(){
-    //Variáveis
-    int ganhou = 1,x,y,cont,velha,ok=0;
+    //Variaveis
+    int ganhou = 1,x,y,pos,cont,velha,ok=0;
     char vez = 'x', jogo [3][3]={
-        {'0','0','0'},
-        {'0','0','0'},
-        {'0','0','0'}
+        {' ',' ',' '},
+        {' ',' ',' '},
+        {' ',' ',' '}
     };
     printf("\tJogo da Velha");
    while(ganhou != 0){
-        printf("\n\n\tMapa\n\n");
-        printf("0 0|0 1|0 2\n");
-        printf("1 0|1 1|1 2\n");
-        printf("2 0|2 1|2 2\n\n");
+        printf("\n\n\tMapa (utiliza o padrao do Num Lock)\n\n");
+        printf("7|8|9\n");
+        printf("-----\n");
+        printf("4|5|6\n");
+        printf("-----\n");
+        printf("1|2|3\n\n");
         //montar o tabuleiro
         printf("\nVez de %c", vez);
         printf("\n\t%c|%c|%c",jogo[0][0],jogo[0][1],jogo[0][2]);//0|0|0
+        printf("\n\t-----");                                    //-----
         printf("\n\t%c|%c|%c",jogo[1][0],jogo[1][1],jogo[1][2]);//0|0|0
+        printf("\n\t-----");                                    //-----
         printf("\n\t%c|%c|%c",jogo[2][0],jogo[2][1],jogo[2][2]);//0|0|0
 
-        //Pegar coordenadas
+        //Pegar a posicao
 
         do{
-            printf("\n\nDigite as coordenadas:");
-            scanf("%d%d",&x,&y);
-            //Verifica se a entrada é válida
-            if(jogo[x][y]!='0'){
-                printf("\n\nEntrada Invalida, a posicao ja foi escolhida, tente novamente\n");
+            printf("\n\nDigite a posicao:");
+            scanf("%d",&pos);
+            //traduzir a posicao
+            switch(pos){
+                case 7:
+                    x = 0;
+                    y = 0;
+                    break;
+                case 8:
+                    x = 0;
+                    y = 1;
+                    break;
+                case 9:
+                    x = 0;
+                    y = 2;
+                    break;
+                case 4:
+                    x = 1;
+                    y = 0;
+                    break;
+                case 5:
+                    x = 1;
+                    y = 1;
+                    break;
+                case 6:
+                    x = 1;
+                    y = 2;
+                    break;
+                case 1:
+                    x = 2;
+                    y = 0;
+                    break;
+                case 2:
+                    x = 2;
+                    y = 1;
+                    break;
+                case 3:
+                    x = 2;
+                    y = 2;
+                    break;
+                default:
+                    break;
+            }
+            //Verifica se a entrada e valida
+            if(jogo[x][y]!=' '){
+                printf("\n\nEntrada Invalida\n");
                 ok = 1;
-            }else if(x>2||x<0||y>2||y<0){
-                printf("\n\nEntrada invalida, os valores nao sao validos");
+            }else if(pos < 1 || pos > 9){
+                printf("\n\nEntrada invalida\n");
                 ok = 1;
             }else{
                 ok = 0;
@@ -45,25 +90,32 @@ int main(){
         //verifica se ganhou
         cont = 0;
         while(cont!=3){
-            if(jogo[cont][0] == jogo[cont][1] && jogo[cont][1] == jogo[cont][2] && jogo[cont][0]!= '0'){//Horizontal
+            if(jogo[cont][0] == jogo[cont][1] && jogo[cont][1] == jogo[cont][2] && jogo[cont][0]!= ' '){//Horizontal
                 printf("\n\n\tO %c GANHOUUU\nParabens!!\n", vez);
                 ganhou = 0;
-            }else if(jogo[0][cont] == jogo[1][cont] && jogo[1][cont] == jogo[2][cont] && jogo[0][cont]!= '0'){//Vertical
+                break;
+            }else if(jogo[0][cont] == jogo[1][cont] && jogo[1][cont] == jogo[2][cont] && jogo[0][cont]!= ' '){//Vertical
                 printf("\n\n\tO %c GANHOUUU\nParabens!!\n", vez);
                 ganhou = 0;
-            }else if(jogo[0][0] == jogo[1][1] && jogo[1][1] == jogo[2][2] && jogo[1][1] != '0'){//Diagonal principal
+                break;
+            }else if(jogo[0][0] == jogo[1][1] && jogo[1][1] == jogo[2][2] && jogo[1][1] != ' '){//Diagonal principal
                 printf("\n\n\tO %c GANHOUUU\nParabens!!\n", vez);
                 ganhou = 0;
-            }else if(jogo[0][2] == jogo[1][1] && jogo[1][1] == jogo[2][0] && jogo[1][1] != '0'){//Diagonal segundária
-
+                break;
+            }else if(jogo[0][2] == jogo[1][1] && jogo[1][1] == jogo[2][0] && jogo[1][1] != ' '){//Diagonal segundaria
+                printf("\n\n\tO %c GANHOUUU\nParabens!!\n", vez);
+                ganhou = 0;
+                break;
             }
             cont++;
         }
         //Verifica se deu velha
-        velha = 1;
+        if(ganhou!=0){
+            velha = 1;
+        }
         for(x = 0;x < 3;x++){
             for(y = 0;y < 3;y++){
-                if(jogo[x][y]=='0'){
+                if(jogo[x][y]==' '){
                     velha = 0;
                 }
             }
@@ -78,14 +130,14 @@ int main(){
         }else{
             vez = 'x';}
    }
+   //Mostra o Tabuleiro no  final
     printf("\n\t%c|%c|%c",jogo[0][0],jogo[0][1],jogo[0][2]);
+    printf("\n\t-----");
     printf("\n\t%c|%c|%c",jogo[1][0],jogo[1][1],jogo[1][2]);
+    printf("\n\t-----");
     printf("\n\t%c|%c|%c\n\n",jogo[2][0],jogo[2][1],jogo[2][2]);
 
-
-
-
-    //pausa o programa após a execução
+    //pausa o programa apos a execucao
     system("pause");
     return 0;
 }
